67fecf139b30cdd077b38c1a35b0aec06d0477cf
jhonnyUrriago <jhonnyurriago997@gmail.com>
2024-07-23 18:24:03 -0300

    aqui hice unas correcciones esteticas y tambien complete el codigo

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class main{
     public static void main(String[] args) {
         System.out.println("");
+        System.out.println("");
 
         System.out.println("Al fin despierto, hace cuanto dormi??... no importa.");
         System.out.println("Por cierto, quien eres tú??, dime tu nombre.");
@@ -39,6 +40,7 @@ public class main{
         Scanner historia=new Scanner(System .in);
         boolean cuento;
         cuento =historia.nextBoolean();
+        System.out.println("");
         if (cuento){
             System.out.println("muy bien,   En un reino lejano, donde los bosques eran tan antiguos como el tiempo y los ríos cantaban melodías olvidadas, vivía un mago llamado Alaric. Alaric era conocido por su sabiduría y poder, pero también por su corazón solitario. Pasaba sus días estudiando antiguos grimorios y sus noches observando las estrellas, buscando respuestas a preguntas que nadie más se atrevía a formular.\r\n" + //
                                 "\r\n" + //
@@ -50,10 +52,36 @@ public class main{
                                 "\r\n" + //
                                 "Lyria sonrió y, con un gesto de su mano, invitó a Alaric a unirse a su danza. Juntos, recorrieron el bosque, descubriendo secretos ocultos y compartiendo historias de sus mundos. Alaric le mostró a Lyria la magia de los elementos, mientras ella le enseñaba los misterios de la naturaleza y la vida.\r\n" + //
                                 "\r\n" + //
-                                "Con el tiempo, su amistad se transformó en algo más profundo.Pero un dia invadieron el bosque y quemaron  todo, incluyendo a Lyria, el mago destrosado nunca ha vuelto a amar");
+                                "Con el tiempo, su amistad se transformó en algo más profundo.Pero un dia invadieron el bosque y quemaron  todo, incluyendo a Lyria, el mago destrosado nunca ha vuelto a amar.");
         }
-        else{System.out.println("ok, esta bien");}
-    
+        else{System.out.println("ok, esta bien, entonces cuentame algo tu.");
+        System.out.println("");
+        Scanner cuento2= new Scanner(System .in);
+        String note;
+        note= cuento2.nextLine();
+        System.out.println("");
+        System.out.println("muy bien ");
+    }
+        System.out.println("");
+
+        System.out.println("y bien "+ nombre+" cuentame algo de ti");
+        System.out.println("responde true o false");
+        System.out.println("");
+
+        Scanner qu= new Scanner(System .in);
+        System.out.println("");       
+        boolean uq;
+        uq= qu.nextBoolean();
+        System.out.println("");
+        if (uq) {
+            Scanner sms= new Scanner(System .in);
+            String sm;
+            sm= sms.nextLine();
+            System.out.println("ummm ya, adios "+nombre+ " espero vernos pronto");
+        }else{
+            System.out.println("entonces, adios "+ nombre+ "espero volvernos a encontrar");
+        }
+
             
     }
 }
